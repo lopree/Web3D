62143f73237a62cfd41b5f2a6b911638db4ae6e8
Rook <zhutong1993@163.com>
2018-07-09 22:49:47 +0800

    注释

--- WebStrom3D/MyD3JS.js
+++ WebStrom3D/MyD3JS.js
@@ -1,11 +1,5 @@
-// var svg = d3.select('body').append("svg").attr('class',"svg-main");
-//
-// var myimage = svg.append('image')
-//     .attr('xlink:href', 'http://lorempixel.com/200/200/').attr("x",0).attr("y",0)
-//     .attr('width', 200)
-//     .attr('height', 200);
-
-d3.xml("rect01.svg")
+// d3 V5版本使用此xml的方式更改：
+d3.xml("./Resources/rect01.svg")
     .then(xml => {
         this.document.body.appendChild(xml.documentElement);
     });
