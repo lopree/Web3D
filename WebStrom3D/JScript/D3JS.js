d3212cdcd6aac9655e66481f0819d8eafce03dc0
Rook <zhutong1993@163.com>
2018-07-10 22:26:57 +0800

    注释

--- WebStrom3D/JScript/D3JS.js
+++ WebStrom3D/JScript/D3JS.js
@@ -1,16 +1,17 @@
 //数据更新的例子（文字）
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-var data02 = ["鱼香肉丝","地三鲜","龙井虾仁","锅包肉","糖醋里脊","西湖醋鱼","叫花鸡","即墨老酒"];
-var svg = d3.select('body').append('div').append('svg').attr("width",300).attr("height",100)
-    .style("border","green solid").attr("class","svg-main");
+var data02 = ["鱼香肉丝", "地三鲜", "龙井虾仁", "锅包肉", "糖醋里脊", "西湖醋鱼", "叫花鸡", "即墨老酒"];
+var svg = d3.select('body').append('div').append('svg').attr("width", 300).attr("height", 100)
+    .style("border", "green solid").attr("class", "svg-main");
 var width = +svg.attr("width"),
     height = +svg.attr("height"),
     g = svg.append("g").attr("transform", "translate(32," + (height / 2) + ")");
 var g2 = svg.append('g')
-            .attr("transform", "translate("+(width/2)+"," + (height / 2) + ")")
-    .attr("id","node00")
-            .append("text");
-function update(data,date) {
+    .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
+    .attr("id", "node00")
+    .append("text");
+
+function update(data, date) {
 
     // DATA JOIN
     // Join new data with old elements, if any.
@@ -22,7 +23,7 @@ function update(data,date) {
     // UPDATE
     // Update old elements as needed.
     text.attr("class", "update");
-    text02.attr("class","update");
+    text02.attr("class", "update");
 
     // ENTER
     // Create new elements as needed.
@@ -32,15 +33,19 @@ function update(data,date) {
     // apply operations to both.
     text.enter().append("text")
         .attr("class", "enter")
-        .attr("x", function(d, i) { return  i * 10; })//文本之间的缩进
+        .attr("x", function (d, i) {
+            return i * 10;
+        })//文本之间的缩进
         .attr("dy", ".5em")
         .merge(text)
-        .text(function(d) { return d; });
+        .text(function (d) {
+            return d;
+        });
 
     text02
         .text(function (d) {
-        return d;
-    });
+            return d;
+        });
 
     // EXIT
     // Remove old elements as needed.
@@ -49,13 +54,13 @@ function update(data,date) {
 }
 
 // The initial display.
-update(alphabet,data02);
+update(alphabet, data02);
 
 // Grab a random sample of letters from the alphabet, in alphabetical order.
-d3.interval(function() {
+d3.interval(function () {
     update(d3.shuffle(alphabet)
         .slice(0, Math.floor(Math.random() * 26))
-        .sort(),d3.shuffle(data02)
+        .sort(), d3.shuffle(data02)
         .slice(0, Math.floor(Math.random() * 26))
-        );
+    );
 }, 500);
--- WebStrom3D/JScript/MyD3JS.js
+++ WebStrom3D/JScript/MyD3JS.js
@@ -6,11 +6,11 @@ d3.xml("./Resources/rect01.svg")
     });
 
 var nodelist = d3.select("#node00").nodes();
-var nodeParent = nodelist.map(function(d){return d.parentNode;});
-var newG = d3.select(nodeParent[0]).attr("transform","translate(500,500)");
-console.log(nodeParent);
+var allParentNode = nodelist.;
+// var newG = d3.select(nodeParent[0]).attr("transform","translate(500,500)");
+console.log(nodelist);
+console.log(allParentNode);
 
 var findidParent = d3.select("#test01").nodes().map(function(d){return d.parentNode;});
 var newTestSVG = d3.select(findidParent[0]).attr("class","importSVG");
-console.log(findidParent);
-console.log(findidParent[findidParent.length-1]);
+
