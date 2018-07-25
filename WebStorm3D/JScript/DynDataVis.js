9230d1c151c7ed2d8afaf8aefd75a30e516e3ec3
lopree <zhutong1993@gmail.com>
2018-07-25 17:40:09 +0800

    Update

--- WebStorm3D/JScript/DynDataVis.js
+++ WebStorm3D/JScript/DynDataVis.js
@@ -4,38 +4,36 @@
 var dataset = [50, 32, 23, 6, 89, 123, 60];
 var newDataArr = [50, 20, 30, 40, 50, 60, 70, 80];
 //饼图的数据
-const pData = [12,13,17];
-var freqData=[
-    {State:'AL',freq:{low:4786, mid:1319, high:249}}
-    ,{State:'AZ',freq:{low:1101, mid:412, high:674}}
-    ,{State:'CT',freq:{low:932, mid:2149, high:418}}
-    ,{State:'DE',freq:{low:832, mid:1152, high:1862}}
-    ,{State:'FL',freq:{low:4481, mid:3304, high:948}}
-    ,{State:'GA',freq:{low:1619, mid:167, high:1063}}
-    ,{State:'IA',freq:{low:1819, mid:247, high:1203}}
-    ,{State:'IL',freq:{low:4498, mid:3852, high:942}}
-    ,{State:'IN',freq:{low:797, mid:1849, high:1534}}
-    ,{State:'KS',freq:{low:162, mid:379, high:471}}
+const pData = [12, 13, 17];
+var freqData = [
+    {State: 'AL', freq: {low: 4786, mid: 1319, high: 249}}
+    , {State: 'AZ', freq: {low: 1101, mid: 412, high: 674}}
+    , {State: 'CT', freq: {low: 932, mid: 2149, high: 418}}
+    , {State: 'DE', freq: {low: 832, mid: 1152, high: 1862}}
+    , {State: 'FL', freq: {low: 4481, mid: 3304, high: 948}}
+    , {State: 'GA', freq: {low: 1619, mid: 167, high: 1063}}
+    , {State: 'IA', freq: {low: 1819, mid: 247, high: 1203}}
+    , {State: 'IL', freq: {low: 4498, mid: 3852, high: 942}}
+    , {State: 'IN', freq: {low: 797, mid: 1849, high: 1534}}
+    , {State: 'KS', freq: {low: 162, mid: 379, high: 471}}
 ];
 console.log(freqData);
-//const pnewData = freqData.each(function(d){d.total = d.freq.high + d.freq.low+d.freq.mid});
-
+freqData.forEach(function (d) {
+    d.total = d.freq.high + d.freq.low + d.freq.mid;
+});
+console.log(freqData);
+const pDATAArray = [];
+for (i = 0; i < freqData.length; i++) {
+    pDATAArray.push(freqData[i].total);
+}
 //1.绘制静态直方图图表
-const Width = 800,Height = 400;
+const Width = 800, Height = 400;
 const TWidth = 400, THeight = 400;
 //在index.html中次脚本的引用晚于MyD3JS.js,因此可以获取到那个脚本中创建的div
 var TableCanvas = d3.select(document.body).select("div")
-    .append("svg").attr("id","Table01").attr("class","TableSVG")
-    .attr("width", Width).attr("height",Height);
-//绘制圆形比例图SVG以及里面的弦
-const pie = d3.pie().value(d=>d);
-const pieData = pie(pData);
-console.log(pieData);
-const arcPath =d3.arc().innerRadius(0).outerRadius(100);
-//根据鼠标的位置，动态更新饼图的数据源
+    .append("svg").attr("id", "Table01").attr("class", "TableSVG")
+    .attr("width", Width).attr("height", Height);
 
-//颜色
-var pColor = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 //获得对应CSS文件中的class中的属性---并且该属性可修改（可读可写）
 const table01 = document.getElementById("Table01");
 //返回字符串,转换成float
@@ -46,29 +44,31 @@ const Table_padding = parseFloat(readPadding);
 const xAxisWidth = TWidth - 2 * Table_padding;
 const yAxisWidth = THeight - 2 * Table_padding;
 
-console.log(readPadding);
-console.log(xAxisWidth);
+//console.log(readPadding);//20px
+//console.log(xAxisWidth);//360
 //上一个直方图的左边距离下一个直方图左边的距离
 const rectStep = 35;
 //直方图宽度,即两个直方图之间的间隔为5
 const rectWidth = 30;
 //绘制坐标轴容器
-var GXAxis = TableCanvas.append('g').attr("transform", "translate(" + Table_padding + "," + (THeight - Table_padding) + ")");
-var GYAxis = TableCanvas.append('g').attr("transform", "translate(" + Table_padding + "," + (THeight - Table_padding - yAxisWidth) + ")");
+var GXAxis = TableCanvas.append('g')
+    .attr("transform", "translate(" + (Table_padding) + "," + (THeight - Table_padding) + ")");
+var GYAxis = TableCanvas.append('g')
+    .attr('id', 'yaxis')
+    .attr("transform", "translate(" + (Table_padding) + "," + (THeight - Table_padding - yAxisWidth) + ")");
+
 //生成比例尺
 //domain()坐标刻度数量，对应后面的像素点，即0-->8px,10-->378px,刻度间距为(378-8)/10
-var xScale = d3.scaleBand().domain(newDataArr.map((d, i) => i + 1)).range([0, xAxisWidth]).padding(0.1);
-var yScale = d3.scaleLinear().domain([0, d3.max(newDataArr)]).rangeRound([yAxisWidth, 0]);
+var xScale = d3.scaleBand().domain(pDATAArray.map((d, i) => i + 1)).range([0, xAxisWidth]).padding(0.1);
+var yScale = d3.scaleLinear().domain([0, d3.max(pDATAArray)]).rangeRound([yAxisWidth, 0]);
 
 
-//生成坐标轴
+//生成坐标轴,以及改变里面元素的属性
 const xAxis = d3.axisBottom(xScale);
 const yAxis = d3.axisLeft(yScale);
-//
 GXAxis.call(xAxis);
-GYAxis.call(yAxis);
+GYAxis.call(yAxis).selectAll('text').attr('class', 'yAxisText');
 
-//数据初始化及更新(伪)
 const RectStyle = obj => {
     obj.attr("fill", "steelblue")
         .attr("x", (d, i) => {
@@ -81,8 +81,8 @@ const RectStyle = obj => {
         .attr("height", (d, i) => {
             return yScale(0) - yScale(d)
         })
-        .on('mouseover',mouseover)
-        .on('mouseout',mouseout);
+        .on('mouseover', mouseover)
+        .on('mouseout', mouseout);
 };
 const TextStyle = obj => {
     obj.attr("class", "DataText")
@@ -122,33 +122,74 @@ function update_Table(newDataSource) {
     }
 
 }
+
+//绘制圆形比例图SVG以及里面的弦
+const pie = d3.pie().value(d => d);
+//获得数据源中的low数据()
+const pielow = d3.pie().value(d => d.freq.low);
+console.log(pielow(freqData).map(d => d.value));
+//获得数据源中freq的value值，返回一个多重数组
+const fre = freqData.map(d => Object.values(d.freq));
+console.log(fre);
+//然后在从多重数组里面分别取出对应的值
+const lowValue = fre.map(d => d[0]);
+const midValue = fre.map((d, i) => d[1]);
+const highValue = fre.map(d => d[2]);
+
+const arcPath = d3.arc().innerRadius(0).outerRadius(100);
+//饼图颜色
+var pColor = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#ab2013", "#76ccc9", "#68cc83"]);
+
 //饼图
-const pStyle = obj=>{
-    obj.attr('stroke','#000')
-        .attr('stroke-width','1px')
-        .attr("transform","translate("+500+","+(Height/2)+")")
-        .attr('d',d=>arcPath(d)).attr('fill',(d,i)=>pColor(i)).attr('class','pTableStyle');
-}
-function init_pTable(dataSource){
-    pStyle(TableCanvas.selectAll(".pTableStyle").data(dataSource).enter().append('path'));
-}
-function updata_pTable(dataSource) {
-    pStyle(TableCanvas.selectAll(".pTableStyle").data(dataSource))
-}
-//鼠标移入移出时触发的事件
-function mouseover(){
-    d3.select(this).attr('fill','blue');
-
-    //updata_pTable()
-}
-function mouseout(){
-    d3.select(this).attr('fill','steelblue')
+const pStyle = obj => {
+    obj.attr('stroke', '#000')
+        .attr('stroke-width', '1px')
+        .attr("transform", "translate(" + 500 + "," + (Height / 2) + ")")
+        .attr('d', d => arcPath(d)).attr('fill', (d, i) => pColor(i)).attr('class', 'pTableStyle')
+        .on('mouseover', mouseover)
+        .on('mouseout', mouseout);
 }
 
-init_Table(dataset);
+function init_pTable(dataSource) {
+    const pieData = pie(dataSource);
+    pStyle(TableCanvas.selectAll(".pTableStyle").data(pieData).enter().append('path'));
+}
 
-update_Table(newDataArr);
-init_pTable(pieData);
+function updata_pTable(newdataSource) {
+    const newPie = pie(newdataSource);
+    const allPTable = TableCanvas.selectAll(".pTableStyle").nodes();
+    const newpTable = TableCanvas.selectAll(".pTableStyle").data(newPie);
+    if (newPie.length < allPTable.length) {
+        pStyle(newpTable);
+        newpTable.exit().remove();
+    } else {
+        pStyle(newpTable.enter().append('path'));
+    }
+}
+
+//鼠标移入移出直方图时触发的事件
+function mouseover(d,i) {
+    d3.select(this).attr('fill', 'blue');
+    //更新饼状图数据
+    const newpData = fre[i];
+    console.log(newpData);
+    updata_pTable(newpData);
+    //
+console.log(i);
+}
+
+function mouseout() {
+    d3.select(this).attr('fill', 'steelblue')
+    init_pTable(pData);
+    //重置饼状图数据
+}
+
+//鼠标移入移出饼图时触发的事件
+
+init_Table(pDATAArray);
+
+//update_Table(newDataArr);
+init_pTable(pData);
 
 
 
