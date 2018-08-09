23feb54ca31752c4e3400ab7216b141345dea1a4
lopree <zhutong1993@gmail.com>
2018-08-09 17:35:05 +0800

    all Three.js

--- WebStorm3D/JScript/threeJS.js
+++ WebStorm3D/JScript/threeJS.js
@@ -1,13 +1,20 @@
-//import * as THREE from "../Core/three/three";
-const scene = new THREE.Scene();
-const  camera = new THREE .PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+import * as THREE from "../Core/three/three";
+let scene = new THREE.Scene();
+let camera = new THREE .PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
 
 let render = new THREE.WebGLRenderer();
 render.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(render.domElement);
+//Create Shape
+let geo = new THREE.BoxGeometry(1,1,1);
+let mat = new THREE.MeshBasicMaterial({color:0xFFFFFF,wireframe:true});
+let cube = new THREE.Mesh(geo,mat);
+scene.add(cube);
+camera.position.z = 3;
 //Game Logic
-let update01 = function(){
-
+let SceneUpdate = function(){
+    cube.rotation.x+=0.01;
+    cube.rotation.y+=3;
 };
 //Draw Scene
 let renderer = function(){
@@ -15,5 +22,10 @@ let renderer = function(){
 };
 //run GameLoop(renderer,update,repeat)
 let GameLoop = function () {
+    SceneUpdate();
+    renderer();
 
+    requestAnimationFrame(GameLoop);
 };
+
+GameLoop();
